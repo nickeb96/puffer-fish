fd710cff666ccb54b9ae55e82d7090c8398f5beb
nickeb96 <nickeb96@gmail.com>
2020-01-26 16:42:56 -0500

    Forgot to change function name

--- functions/_puffer_fish_expand_dots.fish
+++ functions/_puffer_fish_expand_dots.fish
@@ -1,4 +1,4 @@
-function _expanddots -d 'expand ... to ../.. etc'
+function _puffer_fish_expand_dots -d 'expand ... to ../.. etc'
     set -l cmd (commandline --cut-at-cursor)
     switch $cmd[-1]
         case '*..'; commandline --insert '/..'
