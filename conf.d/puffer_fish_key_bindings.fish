729eb5862b2d6ba416ada258ce49abb3510ebb7e
nickeb96 <nickeb96@gmail.com>
2020-02-04 16:20:40 -0500

    Added !! history substitution expansion

--- conf.d/puffer_fish_key_bindings.fish
+++ conf.d/puffer_fish_key_bindings.fish
@@ -1,7 +1,9 @@
 bind . _puffer_fish_expand_dots
+bind ! _puffer_fish_expand_bang
 
 set -l uninstall_event (basename (status -f) .fish){_uninstall}
 
 function $uninstall_event --on-event $uninstall_event
     bind -e .
+    bind -e !
 end
