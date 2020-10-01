0a54703dfea8893baa64bab58b692049e0f8b208
knthmn <knthmn@outlook.com>
2020-10-01 12:13:45 +0800

    Make dots do not expand when the arguement starts with './'

--- functions/_puffer_fish_expand_dots.fish
+++ functions/_puffer_fish_expand_dots.fish
@@ -1,6 +1,8 @@
 function _puffer_fish_expand_dots -d 'expand ... to ../.. etc'
     set -l cmd (commandline --cut-at-cursor)
-    switch $cmd[-1]
+    set -l split (string split ' ' $cmd)
+    switch $split[-1]
+        case './*'; commandline --insert '.'
         case '*..'; commandline --insert '/..'
         case '*'; commandline --insert '.'
     end
