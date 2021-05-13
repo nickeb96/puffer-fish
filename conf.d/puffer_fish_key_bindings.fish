7066fc7758ee6ca44ac11d8f7c399463958c9af7
Kid <44045911+kidonng@users.noreply.github.com>
2021-05-13 22:49:08 +0800

    Don't bind keys in normal mode if not using `fish_default_key_bindings`

--- conf.d/puffer_fish_key_bindings.fish
+++ conf.d/puffer_fish_key_bindings.fish
@@ -1,7 +1,14 @@
-bind . _puffer_fish_expand_dots
-bind . -M insert _puffer_fish_expand_dots
-bind ! _puffer_fish_expand_bang
-bind ! -M insert _puffer_fish_expand_bang
+function _puffer_fish_key_bindings --on-variable fish_key_bindings
+    if test "$fish_key_bindings" = fish_default_key_bindings
+        bind . _puffer_fish_expand_dots
+        bind ! _puffer_fish_expand_bang
+    else
+        bind . -M insert _puffer_fish_expand_dots
+        bind ! -M insert _puffer_fish_expand_bang
+    end
+end
+
+_puffer_fish_key_bindings
 
 set -l uninstall_event (basename (status -f) .fish)_uninstall
 
