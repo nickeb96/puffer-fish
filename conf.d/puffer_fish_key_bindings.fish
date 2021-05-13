bb887665620f7cd9184c329bd0c7c7b76ab48fe3
Kid <44045911+kidonng@users.noreply.github.com>
2021-05-13 23:59:11 +0800

    Erase

--- conf.d/puffer_fish_key_bindings.fish
+++ conf.d/puffer_fish_key_bindings.fish
@@ -1,11 +1,13 @@
 function _puffer_fish_key_bindings --on-variable fish_key_bindings
     if test "$fish_key_bindings" = fish_default_key_bindings
-        bind . _puffer_fish_expand_dots
-        bind ! _puffer_fish_expand_bang
+        set modes default insert
     else
-        bind . -M insert _puffer_fish_expand_dots
-        bind ! -M insert _puffer_fish_expand_bang
+        set modes insert default
     end
+
+    bind --mode $mode[1] . _puffer_fish_expand_dots
+    bind --mode $mode[1] ! _puffer_fish_expand_bang
+    bind --mode $mode[2] --erase . !
 end
 
 _puffer_fish_key_bindings
