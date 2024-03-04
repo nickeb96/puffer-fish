12d062eae0ad24f4ec20593be845ac30cd4b5923
nickeb96 <nickeb96@gmail.com>
2024-03-03 19:23:49 -0500

    Fix expand_dots not working if the cmd line starts with a dash

--- functions/_puffer_fish_expand_dots.fish
+++ functions/_puffer_fish_expand_dots.fish
@@ -1,7 +1,7 @@
 function _puffer_fish_expand_dots -d 'expand ... to ../.. etc'
     set -l cmd (commandline --cut-at-cursor)
     set -l split (string split -- ' ' $cmd)
-    if string match --quiet --regex '^(\.\./)*\.\.$' $split[-1]
+    if string match --quiet --regex -- '^(\.\./)*\.\.$' $split[-1]
         commandline --insert '/..'
     else
         commandline --insert '.'
